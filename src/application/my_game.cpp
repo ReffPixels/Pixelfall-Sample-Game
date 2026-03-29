28748dcb79841f6861d5b55a990675caa42809f3
Reff Pixels <reffpixels@gmail.com>
2026-03-29 19:41:14 +0100

    Hello Triangle OpenGL

--- src/application/my_game.cpp
+++ src/application/my_game.cpp
@@ -2,13 +2,121 @@
 
 #include "application/my_game.h"
 #include <memory>
+#include <iostream>
+#include <glad/glad.h>
 
 std::unique_ptr<Application> Application::create() {
     return std::make_unique<MyGame>();
 }
 
+// Temporary Shader definition
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout(location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+
+// Create vertex array object (VAO) - Required when using OpenGL Core
+unsigned int VAO;
+// Create vertex buffer object (VBO) to send our vertices to the GPU in batches.
+unsigned int VBO;
+// Create vertex shader object
+unsigned int vertexShader;
+// Create shader program
+unsigned int shaderProgram;
 
 bool MyGame::onStart() {
+    std::cout << "START" << std::endl;
+
+    // Define vertices in Normalized Device Coordinates (NDC) 
+    //Keep Z axis to 0 for 2D (Equal depth)
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    // Set up VAO
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Set up VBO - We first need to store the space in memory by declaring a variable, 
+    // then create the buffer and assign it to that space,
+    // and finaly we bind our VBO to the correct buffer type (GL ARRAY BUFFER for Vertex buffers)
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Copy user defined data into the currently bound buffer. 
+    // Set GPU mode to STATIC DRAW since the triangle won't change often.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Set up vertex shader object
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    // Attach shader source code
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Check if compilation was succesful
+    int vertexShaderSuccess;
+    char vertexShaderinfoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexShaderSuccess);
+    if (!vertexShaderSuccess) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, vertexShaderinfoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << vertexShaderinfoLog << std::endl;
+    }
+
+    // Create fragment shader object
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Attach shader source code
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check if compilation was succesful
+    int fragmentShaderSuccess;
+    char fragmentShaderinfoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentShaderSuccess);
+    if (!fragmentShaderSuccess) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, fragmentShaderinfoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << fragmentShaderinfoLog << std::endl;
+    }
+
+    // Set up shader program
+    shaderProgram = glCreateProgram();
+
+    // Link shaders to the shader program
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check if linking was succesful
+    int shaderProgramSuccess;
+    char shaderProgramInfoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &shaderProgramSuccess);
+    if (!shaderProgramSuccess) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, shaderProgramInfoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << shaderProgramInfoLog << std::endl;
+    }
+
+    // Delete shader objects (We don't need them anymore)
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Link Vertex Attributes (Tell the GPU how to interpret the vertex data we provided)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     return true;
 }
 
@@ -16,4 +124,8 @@ void MyGame::onUpdate() {
 }
 
 void MyGame::onRender() {
+    // Use shader program
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
