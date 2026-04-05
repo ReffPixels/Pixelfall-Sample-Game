3c4ce23bb026aad0649f90dd44486d6e713738a8
Reff Pixels <reffpixels@gmail.com>
2026-04-05 04:38:11 +0100

    even more refactors

---
+++ pixelfall/engine/config/shader_config.h
@@ -0,0 +1,19 @@
+// Defines information about shaders.
+
+#pragma once
+// Standard Library
+#include <string_view>
+// Variable Types
+#include "pixelfall/engine/math/vectors.h"
+#include "pixelfall/engine/graphics/color.h"
+
+namespace shader {
+
+    namespace defaults {
+
+        // [TO DO] This code explicitly references OpenGL. 
+        // This should be separated with an ifdef in the future once that is defined in the CmakeLists
+        constexpr std::string_view defaultVertexShader = "assets/shaders/opengl/vertex_shader.glsl";
+        constexpr std::string_view defaultFragmentShader = "assets/shaders/opengl/fragment_shader.glsl";
+    }
+}
--- pixelfall/engine/core/application.cpp
+++ pixelfall/engine/core/application.cpp
@@ -1,12 +1,13 @@
 // Implementation of application.h
 
 #include "pixelfall/engine/core/application.h"
+#include "pixelfall/engine/config/shader_config.h"
 
 void Application::onSetup(std::filesystem::path enginePath, Window& appWindow) {
     this->appWindow = &appWindow;
     defaultShader.emplace(
-        (enginePath / "assets/shaders/vertex_shader.glsl"),
-        (enginePath / "assets/shaders/fragment_shader.glsl")
+        (enginePath / shader::defaults::defaultVertexShader.data()),
+        (enginePath / shader::defaults::defaultFragmentShader.data())
     );
     painter.emplace(*defaultShader, appWindow);
 }
